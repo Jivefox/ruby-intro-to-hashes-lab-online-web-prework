fcf58b15e308796106175e766a5065c7278ae797
Cliff Jurgensen <jivefox.002@gmail.com>
2018-11-27 21:40:50 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,14 +8,15 @@ end
 
 def monopoly
 	monopoly = {
-	  :railroads => {
-	  }
+	  :railroads => 
+	  {}
 	}
 end
 
 def monopoly_with_second_tier
   monopoly = {
-    :railroads => {
+    :railroads => 
+    {
       :pieces => 4}
 	}
 end
